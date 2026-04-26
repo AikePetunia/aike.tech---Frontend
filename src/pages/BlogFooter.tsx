baca1c74f8a10af5f6f93baeaaf9b936f0b1e14f
Aike <dvhb47487963@gmail.com>
2026-04-26 13:15:43 -0300

    Post style finished, need to add some footer kkk

---
+++ src/pages/BlogFooter.tsx
@@ -0,0 +1,14 @@
+import commentBox from 'commentbox.io';
+
+export function BlogFooter() {
+ // const commentBox('5739343649964032-proj')
+    
+    return (
+        <footer>
+            <p>Opinions are my own. </p>
+            <div className="commentbox"></div>
+    </footer>
+)
+}
+
+export default BlogFooter;
--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -3,6 +3,8 @@ import { Link, useParams } from "react-router-dom"
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
 import { loadPostBySlug, type BlogPost as BlogPostType } from "../hooks/loadBlogPost"
+import "./BlogPost.css"
+import { BlogFooter } from "./BlogFooter"
 
 export function BlogPost() {
   const { section, slug } = useParams()
@@ -24,14 +26,30 @@ loadPostBySlug(section as "project" | "random", slug)
   if (error) return <p>{error}</p>
   if (!post) return <p>post not found</p>
 
+  // cambiar titulo segun titulo de post
+  
   return (
-    <main>
-      <Link to="/blog">blog home</Link>
-      <h1>{post.title}</h1>
-          <p>{post.category}</p>
-          <p>cuak</p>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{post.content}</ReactMarkdown>
-    </main>
+    <>
+          <div className="blg-bg-onpost">          </div>      
+      <Link to="/" style={{ color: "red" }}>
+          <button className="blg-go-back-btn">
+              <span className="blg-go-back-text">
+                  go home :3
+             </span>
+          </button>
+      </Link>
+      <div className="blg-post-container">
+          <div className="blg-post-header">
+            <h1>{post.title}</h1>
+            <p>{post.category}</p>
+          </div>
+            <div className="blg-intro-separator"></div>
+          <div className='blg-post-content'>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{post.content}</ReactMarkdown>
+          </div>
+      </div>
+      <BlogFooter />
+    </>
   )
 }
 
